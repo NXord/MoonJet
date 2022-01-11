747f55d29ba142924219057b5e9fd40a2efad32b
NXord <noe.rolland03@gmail.Com>
2022-01-11 15:14:44 +0100

    essaie

--- MoonJet/Game1.cs
+++ MoonJet/Game1.cs
@@ -1,13 +1,25 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended.Content;
+using MonoGame.Extended.Screens;
+using MonoGame.Extended.Screens.Transitions;
+using MonoGame.Extended.Serialization;
+using MonoGame.Extended.Sprites;
+using System;
 
 namespace MoonJet
 {
+    public enum TypeAnimation { walk, decolage, atterisage, idle };
+
     public class Game1 : Game
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        private AnimatedSprite _perso;
+        private TypeAnimation _animation;
+        private Vector2 _positionPerso;
+
 
         public Game1()
         {
@@ -19,6 +31,7 @@ namespace MoonJet
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            _positionPerso = new Vector2(16, 16);
 
             base.Initialize();
         }
@@ -26,16 +39,21 @@ namespace MoonJet
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
+            SpriteSheet animation = Content.Load<SpriteSheet>("Perso.sf", new JsonContentLoader());
+            _perso = new AnimatedSprite(animation);
 
             // TODO: use this.Content to load your game content here
         }
 
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
+            float walkSpeed = (float)gameTime.ElapsedGameTime.TotalSeconds * 100;
+            KeyboardState keyboardState = Keyboard.GetState();
 
-            // TODO: Add your update logic here
+            _animation = TypeAnimation.walk;
+
+            Vector2 deplacement = new Vector2(0, 0);
+            
 
             base.Update(gameTime);
         }
@@ -45,7 +63,9 @@ namespace MoonJet
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
-
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(_perso,_positionPerso);
+            _spriteBatch.End();
             base.Draw(gameTime);
         }
     }
